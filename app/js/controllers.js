df75f850b0647df91ffb5df9ae1b7ecdcdbee6a3
Matheus Sampaio <matheus1401@gmail.com>
2014-06-26 21:56:07 -0300

    Add Trianglify and Frame bar

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,12 +8,12 @@ verbeteControllers.controller('VerbeteListCtrl', ['$scope', '$http', function ($
 
 }]);
 
-
 verbeteControllers.controller('VerbeteDetailCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
     $http.get('verbetes/verbetes.json').success( function (data) {
 	  $scope.verbeteDetail = data[$routeParams.verbeteId - 1];
 	});
 }]);
+
 
 verbeteControllers.directive('navbar', function() {
   return {
@@ -21,3 +21,16 @@ verbeteControllers.directive('navbar', function() {
     templateUrl: 'partials/navbar.html'
   };
 });
+
+verbeteControllers.directive('trianglify', ['$window', function($window) {
+    return function(scope, element, attr) {
+
+      var t = new Trianglify();
+      var pattern = t.generate(2500, 1500);
+
+      element.css({
+       backgroundImage: pattern.dataUrl
+      });
+
+    };
+}]);
