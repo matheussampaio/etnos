f4af868208d585d0673996b254ee747eec21a83a
Fagner Gomes <fagner.gomes@embedded.ufcg.edu.br>
2014-07-15 13:47:52 -0300

    transformando o script em funcao , ainda nao terminado

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-var im = require('imagemagick');
+var 
+im = require('imagemagick'),
+// os object
+os = require('os'),
+// path object
+path = require('path'),
+// fs object
+fs = require('fs'),
+// TMP Folder		
+tmpFolder = path.join(os.tmpDir(), 'historia');
+//create tmpfolder if not exits
+ fs.mkdir('.'+tmpFolder);
+if( ! fs.existsSync(tmpFolder) ) { fs.mkdir(tmpFolder); }
+var getFilePath = function(filepath, callback){
+	var destFilePath = tmpFolder + '/'+filepath.replace(/\//gi, ".") + '.png';
+console.log(destFilePath);
+	im.convert(['-verbose', '.'+filepath, destFilePath],
 
-im.convert(['-verbose', 'files/AL/1/1.TIF', 'files/AL/1/1.png'],
-  function(err, stdout) {
-  if (err)
-    console.error(err);
-  else
-    console.log('stdout:', stdout);
+	  function(err, stdout) {
+	  if (err)
+	    callback(null, err)
+;	  else
+	    callback(destFilePath);
 });
+}
+
+var wipeTmpFolder = function() {
+    if( typeof tmpFolder != 'string' ){ return; }
+    fs.readdir(tmpFolder, function(err, files){
+        for( var i in files ) {
+            fs.unlink(tmpFolder+'/'+files[i]);
+        }
+    });
+}
+
+
+getFilePath('files/AL/1/1.TIF', function(filepath, err){
+	if (err){
+		//console.log(err);
+	}else{
+		console.log(filepath);
+	}
+})
