2533e21365889b1c20ecc4df0d1433497e676d19
Matheus Sampaio <matheus1401@gmail.com>
2014-07-21 15:56:15 -0300

    Melhorado visualização de Verbete

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,24 @@ win.on('new-win-policy', function (frame, url, policy) {
     policy.ignore();
 });
 
+// Wipe the tmpFolder when closing the app (this frees up disk space)
+win.on('close', function(){
+    console.log('Closing app...');
+
+    // @TODO: Wipe temp folder.
+    console.log('Wiping temp folder...');
+
+    win.close(true);
+});
+
+var preventDefault = function(e) {
+    e.preventDefault();
+}
+// Prevent dropping files into the window
+window.addEventListener("dragover", preventDefault, false);
+window.addEventListener("drop", preventDefault, false);
+// Prevent dragging files outside the window
+window.addEventListener("dragstart", preventDefault, false);
 
 var app = angular.module('verbeteApp', [
   'ngRoute',
