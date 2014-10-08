8d8af7809612dbc333864cc281c3b481044a79cd
Fagner Gomes <fagnerngomes@hotmail.com>
2014-10-08 16:46:22 -0300

    add pdfCreator

---
+++ app/js/pdfCreator.js
@@ -0,0 +1,21 @@
+// path Object
+var path = require('path');
+
+var EasyZip = require('easy-zip').EasyZip;
+
+// os Object
+var os = require('os');
+
+// TMP Folder
+var tmpFolder = path.join(os.tmpDir(), 'historia');
+
+// Promise Object
+var Promise = require('promise');
+
+exports.create = function(verbete) {
+
+return new Promise(function (fulfill, reject) {
+
+}
+
+}
