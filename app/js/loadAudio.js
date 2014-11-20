a6773afa4e25f52d9d3745eb483d4250731085fa
Matheus Sampaio <matheus1401@gmail.com>
2014-11-20 20:16:59 -0300

    Fixing Audio

--- app/js/loadAudio.js
+++ app/js/loadAudio.js
@@ -4,9 +4,17 @@ var path = require('path');
 // Promise Object
 var Promise = require('promise');
 
+var config = require('./config');
+
 exports.load = function(verbete) {
 
-  var folderpath = path.resolve(__dirname, verbete.path) + '/';
+  // var folderpath = path.resolve(__dirname, verbete.path) + '/';
+
+  if (config.DEBUG) {
+    var folderpath = path.resolve(__dirname, verbete.path) + '/';
+  } else {
+    var folderpath = path.join(path.dirname(process.execPath), verbete.path);
+  }
   // var distpath = path.join(tmpFolder, verbete.path.slice(6));
   var filename = 'audio.ogg';
 
