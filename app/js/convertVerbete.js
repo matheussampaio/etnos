f2c3d350d9708fab17300b732af9003dbd773e9c
Matheus Sampaio <matheus1401@gmail.com>
2014-12-04 15:53:55 -0300

    sync converts

--- app/js/convertVerbete.js
+++ app/js/convertVerbete.js
@@ -16,7 +16,6 @@ var fs = require('fs');
 var Promise = require('promise');
 
 var mkdirp = require('mkdirp');
-var sys = require('sys')
 var exec = require('child_process').exec;
 
 var isWin = (process.platform === 'win32');
@@ -25,79 +24,91 @@ var isOSX = (process.platform === 'darwin');
 
 
 if (config.DEBUG) {
-  if (isWin)   { imageMagickPath = path.join(__dirname, '/imagemagick-win/convert'); }
-  if (isLinux) { imageMagickPath = path.join(__dirname, '/imagemagick-linux/convert'); }
-  if (isOSX)   { imageMagickPath = path.join(__dirname, '/imagemagick-macos/bin/convert'); }
+    if (isWin)   { imageMagickPath = path.join(__dirname, '/imagemagick-win/convert'); }
+    if (isLinux) { imageMagickPath = path.join(__dirname, '/imagemagick-linux/convert'); }
+    if (isOSX)   { imageMagickPath = path.join(__dirname, '/imagemagick-macos/bin/convert'); }
 } else {
-  if (isWin)   { imageMagickPath = path.join(path.dirname(process.execPath), '/imagemagick-win/convert'); }
-  if (isLinux) { imageMagickPath = path.join(path.dirname(process.execPath), '/imagemagick-linux/convert'); }
-  if (isOSX)   { imageMagickPath = path.join(path.dirname(process.execPath),'/imagemagick-macos/bin/convert'); }
+    if (isWin)   { imageMagickPath = path.join(path.dirname(process.execPath), '/imagemagick-win/convert'); }
+    if (isLinux) { imageMagickPath = path.join(path.dirname(process.execPath), '/imagemagick-linux/convert'); }
+    if (isOSX)   { imageMagickPath = path.join(path.dirname(process.execPath),'/imagemagick-macos/bin/convert'); }
 }
 
 
 
 function convertImage (filepath, destpath) {
-  if (config.DEBUG) {
-    filepath = path.join(__dirname, filepath);
-  } else {
-    filepath = path.join(path.dirname( process.execPath ), filepath);
-  }
+    if (config.DEBUG) {
+        filepath = path.join(__dirname, filepath);
+    } else {
+        filepath = path.join(path.dirname( process.execPath ), filepath);
+    }
 
-  return new Promise(function (fulfill, reject) {
+    return new Promise(function (fulfill, reject) {
 
-   var cmd = [imageMagickPath, '-verbose', filepath, destpath].join(' ');
+        var cmd = [imageMagickPath, '-verbose', filepath, destpath].join(' ');
 
-   exec(cmd, function (err) {
-     if (err) reject(err);
-     else fulfill(destpath);
-   });
+        exec(cmd, function (err) {
+            if (err) {
+                logger.error('error converting ' + destpath, err.stack);
+                reject(err);
+            } else {
+                logger.info('image converted: ' + destpath);
+                fulfill(destpath);
+            }
+        });
 
-      // im.convert(['-verbose', path.join(__dirname, filepath), destpath], function (err) {
-      //     if (err) reject(err);
-      //     else fulfill(destpath);
-      // });
-});
+    });
 }
 
 
 function createFolder (foldername) {
-  logger.info('Creating folder', foldername);
+    logger.info('Creating folder', foldername);
 
-  return new Promise(function (fulfill, reject) {
-   mkdirp(foldername, function (err) {
-    if (err) reject(err);
-    else fulfill();
-  });
- });
+    return new Promise(function (fulfill, reject) {
+        mkdirp(foldername, function (err) {
+            if (err) {
+                logger.error('error creating folder ' + foldername, err.stack);
+                reject(err);
+            } else {
+                logger.info('folder created: ' + foldername);
+                fulfill(foldername);
+            }
+        });
+    });
 }
 
 
-function convertImages (verbetePath, verbeteImages, foldername) {
-  return new Promise.all(verbeteImages.map(function (img) {
-    return convertImage(path.join(verbetePath, img + '.tif'), path.join(foldername, img + '.png'));
-  }));
+function seq() {
+
+}
+
+
+function convertImages (fulfill, reject, verbetePath, verbeteImages, foldername, result) {
+    if (verbeteImages.length == 0) {
+        fulfill(result);
+    } else {
+
+        var filepath = path.join(verbetePath, verbeteImages[0] + '.tif');
+        var destpath = path.join(foldername, verbeteImages[0] + '.png');
+
+        result.push(destpath);
+
+        logger.info('converting ' + verbeteImages[0]);
+
+        convertImage(filepath, destpath).then( function () {
+            convertImages(fulfill, reject, verbetePath, verbeteImages.slice(1), foldername, result);
+        }, function (error) {
+            reject(error);
+        });
+    }
 }
 
 
 exports.convertVerbete = function (verbete) {
-  var foldername = path.join(config.TEMP_FOLDER, verbete.path.slice(3));
+    var foldername = path.join(config.TEMP_FOLDER, verbete.path.slice(3));
 
-  return new Promise(function (fulfill, reject) {
-    createFolder(foldername).then(
-      convertImages(verbete.path, verbete.images, foldername).then(fulfill, reject)
-      );
-  });
-}
-
-
-exports.wipeTmpFolder = function() {
-  if (typeof config.TEMP_FOLDER != 'string') {
-    return;
-  }
-
-  fs.readdir(config.TEMP_FOLDER, function(err, files){
-    for (var i in files) {
-      fs.unlink(path.join(config.TEMP_FOLDER, files[i]));
-    }
-  });
+    return new Promise(function (fulfill, reject) {
+        createFolder(foldername).then(
+            convertImages(fulfill, reject, verbete.path, verbete.images, foldername, [])
+        );
+    });
 }
